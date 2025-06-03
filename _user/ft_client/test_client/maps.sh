0f162385bb46490c21f9ab0d111e9129d63a644b
eq19 <eq19@users.noreply.github.com>
2025-06-03 15:42:06 +0000

    Update fibbo.py

--- _user/ft_client/test_client/maps.sh
+++ _user/ft_client/test_client/maps.sh
@@ -1,7 +1,23 @@
-#!/usr/bin/env bash
-#
+##!/bin/bash
 
-cat $OUTPUT
+#cat $ARTIFACT
+
+# Run IREE and capture ALL output
+RAW_OUTPUT=$(iree-run-module \
+  --module=complex_module.vmfb \
+  --function=serving_default \
+  --input="13xf32=[1,2,3,4,5,6,7,8,9,10,11,12,13]" \
+  --print_statistics=false 2>&1)
+
+# Debug: Save output to file
+echo "$RAW_OUTPUT" > iree_output.txt
+echo "---- RAW OUTPUT ----"
+cat iree_output.txt
+echo "--------------------"
+
+# Pass to decoder
+./float_decoder "$RAW_OUTPUT"
+
 curl -s -X POST \
   -H "Authorization: Bearer ${BEARER}" \
   -H "Content-Type: application/json" \
--- _user/ft_client/test_client/pythonCode/__init__.py
+++ _user/ft_client/test_client/pythonCode/__init__.py
@@ -1,5 +1,6 @@
 __all__ = [
     "jello",      # refers to the 'jello.py' file
-    "utilities",  # refers to the 'utilities' file
+    "compile",    # refers to the 'compile.py' file
+    "utilities",  # refers to the 'utilities.py' file
     #"reverse",   # !!! refers to the 'reverse' function now !!!
 ]
